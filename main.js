e870c3f4cc9d340593cb9975698ce7648925d7e8
André Ciappina <32151463+dehciappina@users.noreply.github.com>
2019-04-01 11:53:12 -0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,6 @@
+
+
+document.addEventListener("DOMContentLoaded", function() {
+    document.querySelector('#a_svg').classList.add('hidden')
+    document.querySelector('#start_anim').classList.add('hidden')
+})
