837807a198e4d223c54bfe6d7bb7629fd37bc4ee
André Ciappina <32151463+dehciappina@users.noreply.github.com>
2019-04-02 00:35:54 -0300

    Add files via upload

--- scripts/main-page-start.js
+++ scripts/main-page-start.js
@@ -1,5 +1,15 @@
 
 document.addEventListener("DOMContentLoaded", function() {
+    let aLine = document.querySelectorAll('#a_line')
+
     document.querySelector('#a_svg').classList.add('hidden')
     document.querySelector('#start_anim').classList.add('hidden')
+
+    setTimeout(() => {
+        document.querySelector('#up_line').style.transform = 'scaleY(' + 1 + ')'
+        
+        for(i = 0; i < aLine.length; ++i) {
+            aLine[i].style.transform = 'scaleX(' + 1 + ')'
+        }
+    }, 1200);
 })
